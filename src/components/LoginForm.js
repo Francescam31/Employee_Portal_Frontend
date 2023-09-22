8fefac494ddde46b496dda432155795b7c793001
Francesca Marshall <francescam3105@gmail.com>
2023-09-22 10:26:15 +0100

    created states for email and password in loginPage

---
+++ src/components/LoginForm.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const LoginForm = () => {
+    
+    const [email ,setEmail] = useState(
+        {
+            email:""
+        }
+    )
+
+    const [password ,setPassword] = useState(
+        {
+            password:""
+        }
+    )
+
+}  
