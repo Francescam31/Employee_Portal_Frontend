9ebc2e3904e8c7f4784614a9583c0d4bb8bc2034
Francesca Marshall <francescam3105@gmail.com>
2023-09-22 14:43:02 +0100

    Added return for portal container

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,14 +1,15 @@
 const Employee = ({employee}) =>{
 
-
+console.log(employee);
 
 
 return(
     <div className="Employee">
         <h2>{employee.id}</h2>
-        <h3 className="Employee-name">{employee.name}</h3>
-        <h3 className="Employee-email">{employee.email}</h3>
-        <h3 className="Employee-department-name">{employee.department.name}</h3>
+        {/* <h3 className="Employee-name">{employee.name}</h3> */}
+        {/* <h3 className="Employee-email">{employee.email}</h3> */}
+        {/* <h3 className="Employee-department-name">{employee.department.name}</h3> */}
+        {/* <p>{employee.contactNumber}</p> */}
     </div>
 )
 
