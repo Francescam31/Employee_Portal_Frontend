28f7c884da3b5134775969c30b48d0ace2ebddce
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-09-22 14:48:11 +0100

    moved the conditional render from portal to employee

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,14 +2,17 @@ const Employee = ({employee}) =>{
 
 console.log(employee);
 
+if(!employee){
+    return <p>loading data</p>
+}
 
 return(
     <div className="Employee">
         <h2>{employee.id}</h2>
-        {/* <h3 className="Employee-name">{employee.name}</h3> */}
-        {/* <h3 className="Employee-email">{employee.email}</h3> */}
-        {/* <h3 className="Employee-department-name">{employee.department.name}</h3> */}
-        {/* <p>{employee.contactNumber}</p> */}
+        <h3 className="Employee-name">{employee.name}</h3>
+        <h3 className="Employee-email">{employee.email}</h3>
+        <h3 className="Employee-department-name">{employee.department.name}</h3>
+        <p>{employee.contactNumber}</p>
     </div>
 )
 
