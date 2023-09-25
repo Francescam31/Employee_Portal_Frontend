ca4e97f73c4cf5fcf05b13cde5c93e9f014ada08
amelieboydshire <aboydshire10@gmail.com>
2023-09-25 17:48:18 +0100

    adding background styling to CSS

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -46,23 +46,32 @@ const handleFormSubmit = async (event) => {
 }
 
     return (
-        <div className="login-container">
-            <div className="login-form">
+        <div>
+            <div className="login-container">
+                <div className="login-form">
+            {/* <img className="background-image" src="/Rainforest background.jpg"></img> */}
             <img className="login-logo" src="/rainforest retail.png"></img>
             <h1 className="login-title">Account Login</h1>
             <p className="welcome-text">Welcome Back</p>
             <form onSubmit={handleFormSubmit}>
                 <p className="email-prompt">Enter your email</p>
-                <input name="email" placeholder="Email" onChange={handleChangeEmail} value={loginEmail}/>
+                <input className="input" name="email" placeholder="Email" onChange={handleChangeEmail} value={loginEmail}/>
                 <p className="password-prompt">Enter your password</p>
-                <input type="password" name="password" placeholder="Password" onChange={handleChangePassword} value={loginPassword}/>
+                <input className="input" type="password" name="password" placeholder="Password" onChange={handleChangePassword} value={loginPassword}/>
+                <hr className="hr"></hr>
                 <Link className="forgotten-password-button">Forgotten password</Link>
+                <hr className="hr"></hr>
                 <button className="login-button" type="submit">Log In</button>
             </form>
             </div>
+            
+            <div className="footer">
             <p>Rainforest Retails is using cookies to collect anonymous visitor analytics. | PDF | Terms and Conditions, Privacy Policy | Mobile view
 ©RainforestretailEmployee.com</p>
+            </div>
+            </div>
         </div>
+       
     )
 }  
 
