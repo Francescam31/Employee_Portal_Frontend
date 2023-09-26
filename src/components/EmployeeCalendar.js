ba4a0e04bf7292b74c389f6dbd3296cec5361585
Francesca Marshall <francescam3105@gmail.com>
2023-09-26 14:21:52 +0100

    calender edits

--- src/components/EmployeeCalendar.js
+++ src/components/EmployeeCalendar.js
@@ -3,7 +3,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
 
 
-const EmployeeCalendar = ({employee, calendar}) =>{
+const EmployeeCalendar = ({employee}) =>{
 
     const localizer = momentLocalizer(moment);
 
