b0ff74c4f047cec5e5a5d99253474dc5f141dfd5
Blezzy dela Cruz <blezzydelacruz@Blezzys-MacBook-Air.local>
2023-12-07 21:16:45 +0000

    created resources folder and started DocumentsModal

---
+++ src/resources/modals/DocumentsModal.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {useState} from "react"
+
+const DocumentsModal = () => {
+    const [documents, setDocuments] = useState([]);
+
+}
+
+export default DocumentsModal;
