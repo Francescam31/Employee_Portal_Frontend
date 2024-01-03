a44bf4f4b452885808d256b67024a73775c71d73
Blezzy dela Cruz <blezzydelacruz@Blezzys-MacBook-Air.local>
2024-01-03 10:16:42 +0000

    attempted to add article carousel

--- src/resources/Articles.js
+++ src/resources/Articles.js
@@ -1,19 +1,29 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
 const Articles = () => {
+    const articlesCarousel = [
+        {
+            title: "Startup Aims To Prevent Workers’ Back Injuries With ‘Exosuit’ That’s Practical Rather Than ‘Iron Man’ Flashy",
+            link: 'https://www.forbes.com/sites/amyfeldman/2023/12/13/startup-prevents-workers-back-injuries-with-exosuit-thats-practical-rather-than-iron-man-flashy/'
+        },
+        {
+            title: "article 2",
+            link: 'https://www.forbes.com/sites/amyfeldman/2023/12/13/startup-prevents-workers-back-injuries-with-exosuit-thats-practical-rather-than-iron-man-flashy/'
+        },
+        {
+            title: "article 3",
+            link: 'https://www.forbes.com/sites/amyfeldman/2023/12/13/startup-prevents-workers-back-injuries-with-exosuit-thats-practical-rather-than-iron-man-flashy/'
+        }
+    ]
     return (
-        <div className="articles-carousel">
-            <div className="article-1">
-                <Link to="https://www.forbes.com/sites/amyfeldman/2023/12/13/startup-prevents-workers-back-injuries-with-exosuit-thats-practical-rather-than-iron-man-flashy/">Article Title</Link>
-            </div>
-            <div className="article-2">
-                <Link to="https://www.forbes.com/sites/amyfeldman/2023/12/13/startup-prevents-workers-back-injuries-with-exosuit-thats-practical-rather-than-iron-man-flashy/">Article Title</Link>
-            </div>
-            <div className="article-3">
-                <Link to="https://www.forbes.com/sites/amyfeldman/2023/12/13/startup-prevents-workers-back-injuries-with-exosuit-thats-practical-rather-than-iron-man-flashy/">Article Title</Link>
-            </div>
+        <div className="articles-container">
+            {articlesCarousel.map((article, index) => (
+                <div key={index} className="article">
+                    <h2 className="article-title">{article.title}</h2>
+                    <a href={articles.link}>Learn More</a>
+                </div>
+            ))}
          </div>
-)
+);
 }
-
 export default Articles;
